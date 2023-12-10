102647aefc8307eb85f8ebc6c57f8950f67a0270
odinokun <odinokun@gmail.com>
2023-12-10 17:44:50 -0500

    feat:  loading and not-found pages was  created

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,9 @@
-export default function Loading () {
+import styles from './loading.module.scss';
+
+export default function Loading() {
   return (
-    <div>
-      <h1>Loading ...</h1>
+    <div className={styles.loading}>
+      <h1 className={styles.loading__title}>Loading ...</h1>
     </div>
   );
 }
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,15 @@
-export default function NotFound () {
+import { LinkButton } from '@/common/components/LinkButton/LinkButton';
+import styles from './not-found.module.scss';
+
+export default function NotFound() {
   return (
-    <div>
-      <h1>Page Not Found</h1>
+    <div className={styles.notFound}>
+      <h1 className={styles.notFound__title}>Page Not Found</h1>
+      <LinkButton
+        href="/"
+        title="Back to home"
+        btnColor="light"
+      />
     </div>
   );
 }
