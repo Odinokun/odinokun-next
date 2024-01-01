3abf99017b362bdef65cb89d282bec2d385e1209
odinokun <odinokun@gmail.com>
2024-01-01 18:09:25 -0500

    feat: cv was updated

--- src/static/links.ts
+++ src/static/links.ts
@@ -1,4 +1,4 @@
-export const portfolioLink = '/front_end_developer_Dima_Hordiienko.pdf';
+export const portfolioLink = '/Dima_Hordiienko_front_end_developer.pdf';
 export const githubLink = 'https://github.com/Odinokun';
 export const linkedinLink = 'https://www.linkedin.com/in/dmitry-gordienko';
-export const emailLink = 'mailto:odinokun@gmail.com';
+export const emailLink = 'mailto:dima.hordiienko.ca@gmail.com';
